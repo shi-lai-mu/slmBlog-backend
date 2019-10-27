05a1889201d3809f7c4e50e8217de372de276284
shi-lai-mu <478889187@qq.com>
2019-10-27 18:31:03 +0800

    feat: update status code

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,9 +4,9 @@ declare module 'egg' {
   interface mysql {
     get(tableName: String, find: {}): Promise<Any>
     insert(tableName: String, find: {}): Promise<Any>
-    select(sql: String, find: {}): Promise<Any>
+    select(sql: String, find?: {}): Promise<Any>
     update(sql: String, find: {}): Promise<Any>
-    query(sql: String, values: Any[]): Promise<Any>
+    query(sql: String, values?: Any[]): Promise<Any>
   }
   interface Application {
     mysql: mysql;
